916fe0c279d40a84ee9d5d3698ba0fdb1d02fca4
boloto1979 <pedro.lima1979@hotmail.com>
2024-02-22 21:21:42 -0300

    feat: Integrate MalwareAnalysisService for File Uploads
    This commit represents a significant update to our file upload handling mechanism within the FileUploadController. We have transitioned from relying on the external VirusTotalService for malware analysis to utilizing our custom-built MalwareAnalysisService. This change allows for more granular control over the analysis process and enables us to implement specialized malware detection techniques tailored to our application's needs.
    
    Changes include:
    - Replacing the VirusTotalService dependency with MalwareAnalysisService in the FileUploadController.
    - Updating the  method to use the MalwareAnalysisService for scanning uploaded files. The method now evaluates the safety of files based on the analysis results provided by the new service.
    - Removing the  method, as it is no longer necessary with the shift to the MalwareAnalysisService.
    - Enhancements to the MalwareAnalysisService include the implementation of YARA rules compilation for improved performance, and a refined approach to code disassembly analysis to target executable files more effectively.
    - Additional adjustments to ensure that the controller and service communicate properly, providing detailed analysis feedback in the response to the file uploader.
    
    This update marks a pivotal step in enhancing our application's security posture by leveraging in-house capabilities for malware detection and analysis.
    
    Signed-off-by: [Pedro/boloto1979] pedro.lima1979@hotmail.com

--- resources/views/components/update.blade.php
+++ resources/views/components/update.blade.php
@@ -12,12 +12,12 @@
 <div id="analysis-result" class="fixed inset-0 flex items-center justify-center hidden bg-black bg-opacity-50" style="backdrop-filter: blur(5px);">
     <div class="w-full max-w-md p-6 bg-white rounded-lg shadow-lg">
         <div class="flex items-center justify-between mb-4">
-            <h3 class="text-xl font-bold">Resultado da Análise</h3>
+            <h3 class="text-xl font-bold">Analysis result</h3>
             <button onclick="closePopup()" class="text-gray-400 transition duration-150 ease-in-out hover:text-gray-600">
                 <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
             </button>
         </div>
-        <p id="result-text" class="text-gray-600">Seu arquivo está sendo analisado...</p>
+        <p id="result-text" class="text-gray-600">Your file is being analyzed...</p>
     </div>
 </div>
 
